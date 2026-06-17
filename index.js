3e1a2391890dd13e0d299d8bebc5c8c1c3c0d32d
shwigy <floressj04@icloud.com>
2026-06-17 00:01:27 -0400

    adjusted 'changeBackgroundColor' function to allow varied color cycling

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // Handle Button Clicks
 
+// Color array to allow color cycling
+const colors = [
+  "rgb(255, 38, 0)",   // red
+  "rgb(0, 128, 255)",  // blue
+  "rgb(0, 255, 115)",  // green
+];
+let colorIndex = 0;
+
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
-  document.body.style.backgroundColor = "rgb(34, 139, 34)";
-  document.body.style.backgroundColor = "rgb(34, 139, 34)";
-  document.body.style.backgroundColor = "rgb(34, 139, 34)";
+  document.body.style.backgroundColor = colors[colorIndex];
+  colorIndex += 1;
+
+  // repeat array list cycle
+  if (colorIndex >= colors.length) {
+    colorIndex = 0;
+  }
 }
 
 // Function to reset the background color when the body is double-clicked
